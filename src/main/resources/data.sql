2c5be6d0add8b0bc947f38c6a4766e630ab4bb02
student <student@FK-0832>
2025-07-07 11:34:00 +0900

    最初の更新

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+-- ドリンクテーブルデータ
+INSERT INTO drink(id, name, price,category,amount,factory) VALUES(1, '厳選静岡茶', 250,'お茶',650,'イ・トウエン');
+INSERT INTO drink(id, name, price,category,amount,factory) VALUES(2, 'BASS',120,'コーヒー',500,'SONTORY');
+INSERT INTO drink(id, name, price,category,amount,factory) VALUES(3, 'いろばす', 100,'水',500,'コ・カコーラ');
+INSERT INTO drink(id, name, price,category,amount,factory) VALUES(4, 'クラフトコーラ', 120,'清涼飲料水',350,'コ・カコーラ');
+INSERT INTO drink(id, name, price,category,amount,factory) VALUES(5, 'エベレストの水', 200,'水',1000,'ネパール株式会社');
+INSERT INTO drink(id, name, price,category,amount,factory) VALUES(6, 'おいしいお茶', 120,'お茶',600,'コ・カコーラ');
+INSERT INTO drink(id, name, price,category,amount,factory) VALUES(7, 'ポカエリアス', 130,'清涼飲料水',550,'コ・カコーラ');
+INSERT INTO drink(id, name, price,category,amount,factory) VALUES(8, 'GEORGIAN', 150,'コーヒー',300,'コ・カコーラ');
+
+
+
+
+
+
+-- ユーザーテーブルデータ
+INSERT INTO users(id, name, password,email,address,status) VALUES(1, '柳田悠岐','himitu','gita@aaa','福岡県福岡市中央区地行浜2-2-2', 2);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,26 @@
+--各テーブル削除
+DROP TABLE IF EXISTS drink;
+DROP TABLE IF EXISTS users;
+
+
+
+CREATE TABLE drink
+(
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	price INTEGER,
+	category TEXT,
+	amount INTEGER,
+	factory TEXT
+);
+
+CREATE TABLE users
+(
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	password TEXT,
+	email TEXT,
+	address TEXT,
+	status INTEGER
+	
+);
